a23ac33545d85d387a4f673f3b49b580a4cc45d9
tattubhattu <tanyabhatnagar022003@gmail.com>
2025-09-04 00:00:53 +0530

    make auth middleware

--- server/routes/companyRoutes.js
+++ server/routes/companyRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { ChangeJobApplicationsStatus, changeVisibility, getComapanyData, getComapanyPostedJobs, getCompanyJobApplictions, loginComapany, postJob, registerComapany } from '../controllers/companyController.js'
 import upload from '../config/multer.js'
+import { protectCompany } from '../middlewares/authMiddleware.js'
 
 const router = express.Router()
 
@@ -11,21 +12,21 @@ router.post('/register',upload.single('image'), registerComapany)
 router.post('/login',loginComapany)
 
 //get company data 
-router.get('/company',getComapanyData)
+router.get('/company',protectCompany, getComapanyData)
 
 //post a job
-router.post('/post-job',postJob)
+router.post('/post-job',protectCompany, postJob)
 
 //get apllicants data of comapny
-router.get('/applicants',getCompanyJobApplictions)
+router.get('/applicants',protectCompany, getCompanyJobApplictions)
 
 //get company job list 
-router.get('/list-jobs',getComapanyPostedJobs)
+router.get('/list-jobs',protectCompany, getComapanyPostedJobs)
 
 //change applications status
-router.post('/change-status',ChangeJobApplicationsStatus)
+router.post('/change-status',protectCompany, ChangeJobApplicationsStatus)
 
 //change application visibility
-router.post('/change-visibility',changeVisibility)
+router.post('/change-visibility',protectCompany, changeVisibility)
 
 export default router
