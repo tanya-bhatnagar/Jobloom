30546c2a805cbe11ba5abd3d90328d0bb1a63299
tattubhattu <tanyabhatnagar022003@gmail.com>
2025-09-17 16:51:47 +0530

    changes

--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -6,7 +6,7 @@ import Navbar from '../components/Navbar'
 import { assets } from '../assets/assets'
 import kconvert from 'k-convert'
 import moment from 'moment';
-import JobCard from '../components/jobCard'
+import JobCard from "../components/JobCard";
 import Footer from '../components/Footer'
 import axios from 'axios'
 import { toast } from 'react-toastify'
