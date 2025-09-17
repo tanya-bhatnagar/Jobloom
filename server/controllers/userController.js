863f7b19c5e8ca32784e5fe7bfa24231293b75c4
tattubhattu <tanyabhatnagar022003@gmail.com>
2025-09-17 16:20:51 +0530

    fix

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,5 @@
 import Job from "../models/Job.js"
-import JobApplication from "../models/jobApplication.js"
+import JobApplication from "../models/JobApplication.js"
 import User from "../models/User.js"
 import {v2 as cloudinary} from 'cloudinary'
 
